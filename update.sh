6e3cb5639ae70513f9bd9a9b34949141c70ae1c4
ramenu <github.unspeakably@slmail.me>
2023-05-23 21:53:29 +0000

    add docs and only build updated pkgs

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_NAME="rm-extra"
+REPO_NAME='rm-extra'
 archs=('x86_64' 'i686' 'pentium4' 'armv7h' 'aarch64' 'any')
 
 is_architecture() {
@@ -25,10 +25,23 @@ done
 for dir in *; do
 	is_arch=$(is_architecture "$dir")
 	if [[ ! "$is_arch" ]]; then
+
+		# Skip if not a directory
 		if [[ ! -d ./"$dir" ]]; then
 			continue
 		fi
+
+		# Does this package have no updates available?
+		# If not, skip it
+		if [[ -z "$(python ./checkpkgs.py "$dir")" ]]; then
+			echo "No updates available for ${dir}, skipping"
+			continue
+		fi
+
 		cd ./"$dir"
+
+		# Convert the package's compatible architectures
+		# into an array
 		pkg_archs=( $(awk -F "'" '
 			/^arch=/ {
 				for (i=2; i<NF; i+=2) {
@@ -37,6 +50,7 @@ for dir in *; do
 			}
 			' ./PKGBUILD) )
 		
+		# Remove all outdated packages
 		echo Removing all packages from "$dir"
 		rm ./*.pkg.tar.zst > /dev/null 2>&1
 
@@ -44,6 +58,7 @@ for dir in *; do
 		makepkg -f || exit
 
 		for pkg_arch in "${pkg_archs[@]}"; do
+			# Is this an unknown architecture?
 			if [[ ! -d "../$pkg_arch" ]]; then
 				echo Unknown architecture for "$dir": "$pkg_arch"
 				exit
@@ -66,6 +81,8 @@ for arch in "${archs[@]}"; do
 		cd ..
 		continue
 	fi
+
+	# Add the database files..
 	repo-add "$REPO_NAME".db.tar.gz ./*.pkg.tar.zst
 	rm ./"$REPO_NAME".db ./"$REPO_NAME".files
 	mv ./"$REPO_NAME".db.tar.gz ./"$REPO_NAME".db
